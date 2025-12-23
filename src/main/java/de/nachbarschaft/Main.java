91ddcf208d77e2a22687d9f481a30f46e884d608
starkmaxim31-beep <starkmaxim31@gmail.com>
2025-12-23 14:10:27 +0100

    Refactor Main class for Kapitel management

--- src/main/java/de/nachbarschaft/Main.java
+++ src/main/java/de/nachbarschaft/Main.java
@@ -1,63 +1,36 @@
 package de.nachbarschaft;
 
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 
-    private ChapterManager chapterManager;
+    private static Main instance;
+    private KapitelManager kapitelManager;
 
     @Override
     public void onEnable() {
+        instance = this;
+
         saveDefaultConfig();
-        chapterManager = new ChapterManager(this);
-        getLogger().info("Nachbarschaft Kapitel-System aktiviert.");
+
+        kapitelManager = new KapitelManager(this);
+
+        getCommand("kapitel").setExecutor(new KapitelCommand(kapitelManager));
+
+        getLogger().info("Phase 2 Story System aktiviert!");
+    }
+
+    public static Main getInstance() {
+        return instance;
+    }
+
+    public KapitelManager getKapitelManager() {
+        return kapitelManager;
     }
 
     @Override
     public void onDisable() {
-        getLogger().info("Nachbarschaft Plugin gestoppt.");
-    }
-
-    @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-
-        if(cmd.getName().equalsIgnoreCase("kapitel")) {
-
-            if(!(sender instanceof Player)) {
-                sender.sendMessage("Nur Spieler können Kapitel benutzen.");
-                return true;
-            }
-
-            Player p = (Player) sender;
-
-            if(args.length == 0) {
-                int kapitel = chapterManager.getChapter(p);
-                p.sendMessage("§aDein aktuelles Kapitel: §e" + kapitel);
-                return true;
-            }
-
-            if(args[0].equalsIgnoreCase("start")) {
-                chapterManager.setChapter(p, 1);
-                p.sendMessage("§6Kapitel §e1 §6gestartet!");
-                return true;
-            }
-
-            if(args[0].equalsIgnoreCase("weiter")) {
-                int chap = chapterManager.getChapter(p);
-                chap++;
-                chapterManager.setChapter(p, chap);
-                p.sendMessage("§aDu bist jetzt in Kapitel §e" + chap);
-                return true;
-            }
-
-            p.sendMessage("§cBenutze: /kapitel | /kapitel start | /kapitel weiter");
-            return true;
-        }
-
-        return false;
+        getLogger().info("Phase 2 System deaktiviert.");
     }
 }
 
