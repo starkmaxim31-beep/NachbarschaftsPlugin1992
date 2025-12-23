bef30f253402288291b054b2ade687ddd44154af
starkmaxim31-beep <starkmaxim31@gmail.com>
2025-12-23 14:02:40 +0100

    Implement KapitelCommand for player commands

--- src/main/java/de/nachbarschaft/KapitelCommand.java
+++ src/main/java/de/nachbarschaft/KapitelCommand.java
@@ -1,1 +1,54 @@
+package de.nachbarschaft;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class KapitelCommand implements CommandExecutor {
+
+    private final KapitelManager manager;
+
+    public KapitelCommand(KapitelManager manager) {
+        this.manager = manager;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("Nur Spieler!");
+            return true;
+        }
+
+        Player p = (Player) sender;
+
+        if (args.length == 0) {
+            p.sendMessage("§e/kapitel start");
+            p.sendMessage("§e/kapitel weiter");
+            p.sendMessage("§e/kapitel info");
+            return true;
+        }
+
+        switch (args[0].toLowerCase()) {
+            case "start":
+                manager.startStory(p);
+                break;
+
+            case "weiter":
+                manager.nextKapitel(p);
+                break;
+
+            case "info":
+                manager.info(p);
+                break;
+
+            default:
+                p.sendMessage("§cUnbekannter Befehl!");
+                break;
+        }
+
+        return true;
+    }
+}
 
