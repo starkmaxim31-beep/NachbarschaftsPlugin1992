4c0de50faafa60fe3d86371c43a5fb7b557de993
starkmaxim31-beep <starkmaxim31@gmail.com>
2025-12-28 11:09:08 +0100

    Update Main.java

--- src/main/java/de/nachbarschaft/Main.java
+++ src/main/java/de/nachbarschaft/Main.java
@@ -11,6 +11,8 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.metadata.FixedMetadataValue;
+import net.citizensnpcs.api.CitizensAPI;
+import org.bukkit.entity.EntityType;
 
 import java.util.Arrays;
 
