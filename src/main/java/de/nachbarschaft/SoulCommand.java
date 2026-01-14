e36bf7c2cbbf0d37ef01fc484635af26dcbdc863
starkmaxim31-beep <starkmaxim31@gmail.com>
2026-01-14 10:53:31 +0100

    Implement SoulCommand for sword and bow
    Added command handling for SoulSword and SoulBow.

--- src/main/java/de/nachbarschaft/SoulCommand.java
+++ src/main/java/de/nachbarschaft/SoulCommand.java
@@ -1,1 +1,30 @@
+package de.nachbarschaft.soul;
+
+import de.nachbarschaft.soul.weapon.SoulBow;
+import de.nachbarschaft.soul.weapon.SoulSword;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class SoulCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if (!(sender instanceof Player player)) return true;
+
+        if (args.length != 1) {
+            player.sendMessage("§c/soul sword | bow");
+            return true;
+        }
+
+        switch (args[0].toLowerCase()) {
+            case "sword" -> player.getInventory().addItem(SoulSword.create());
+            case "bow" -> player.getInventory().addItem(SoulBow.create());
+        }
+
+        return true;
+    }
+}
 
