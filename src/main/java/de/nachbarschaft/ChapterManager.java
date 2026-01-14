e8b6383793b6fb6bee1c6985a64d6029ff2b9d4f
starkmaxim31-beep <starkmaxim31@gmail.com>
2026-01-14 10:46:43 +0100

    Update ChapterManager.java

--- src/main/java/de/nachbarschaft/ChapterManager.java
+++ src/main/java/de/nachbarschaft/ChapterManager.java
@@ -1,23 +1,17 @@
-package de.nachbarschaft;
+package de.nachbarschaft.chapter;
 
-import org.bukkit.entity.Player;
-import org.bukkit.configuration.file.FileConfiguration;
+import java.util.HashMap;
+import java.util.UUID;
 
 public class ChapterManager {
 
-    private final Main plugin;
+    private static final HashMap<UUID, Integer> chapters = new HashMap<>();
 
-    public ChapterManager(Main plugin) {
-        this.plugin = plugin;
+    public static void setChapter(UUID uuid, int chapter) {
+        chapters.put(uuid, chapter);
     }
 
-    public int getChapter(Player p) {
-        return plugin.getConfig().getInt("players." + p.getUniqueId() + ".chapter", 0);
-    }
-
-    public void setChapter(Player p, int chapter) {
-        FileConfiguration config = plugin.getConfig();
-        config.set("players." + p.getUniqueId() + ".chapter", chapter);
-        plugin.saveConfig();
+    public static int getChapter(UUID uuid) {
+        return chapters.getOrDefault(uuid, 1);
     }
 }
