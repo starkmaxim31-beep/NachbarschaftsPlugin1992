7e62d77540a62c69f0436dd2b4e016ffe10f44d1
starkmaxim31-beep <starkmaxim31@gmail.com>
2026-04-08 14:53:37 +0200

    Implement plugin startup and shutdown logging

--- src/main/java/de/nachbarschaft/Main.java
+++ src/main/java/de/nachbarschaft/Main.java
@@ -1,1 +1,16 @@
+package de.nachbarschaft;
 
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("NachbarschaftsPlugin wurde gestartet!");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("NachbarschaftsPlugin wurde gestoppt!");
+    }
+}
