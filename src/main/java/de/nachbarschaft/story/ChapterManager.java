3210077a46d034b147c0be4861d107e0bd5b0030
starkmaxim31-beep <starkmaxim31@gmail.com>
2026-04-08 21:47:58 +0200

    Update ChapterManager.java

--- src/main/java/de/nachbarschaft/story/ChapterManager.java
+++ src/main/java/de/nachbarschaft/story/ChapterManager.java
@@ -9,6 +9,11 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import de.nachbarschaft.story.events.StoryTriggerManager;
+private StoryTriggerManager triggerManager = new StoryTriggerManager();
+
 public class ChapterManager {
 
     private JavaPlugin plugin;
@@ -41,11 +46,21 @@ public class ChapterManager {
         return chapterMap.getOrDefault(uuid, 1);
     }
 
-    public void setChapter(UUID uuid, int chapter) {
-        chapterMap.put(uuid, chapter);
-        config.set(uuid.toString(), chapter);
-        save();
+ public void setChapter(UUID uuid, int chapter) {
+
+    chapterMap.put(uuid, chapter);
+
+    config.set(uuid.toString(), chapter);
+
+    save();
+
+    Player player = Bukkit.getPlayer(uuid);
+
+    if (player != null) {
+        triggerManager.checkChapterTrigger(player, chapter);
     }
+
+}
 
     public void loadChapters() {
         for (String key : config.getKeys(false)) {
