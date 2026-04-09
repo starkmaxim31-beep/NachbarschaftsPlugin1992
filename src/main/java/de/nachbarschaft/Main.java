c1614e0e880d30272caafdb18bf3f52836160d60
starkmaxim31-beep <starkmaxim31@gmail.com>
2026-04-09 05:49:20 +0200

    Refactor Main class for better readability and structure

--- src/main/java/de/nachbarschaft/Main.java
+++ src/main/java/de/nachbarschaft/Main.java
@@ -5,45 +5,83 @@ import org.bukkit.plugin.java.JavaPlugin;
 import de.nachbarschaft.story.ChapterManager;
 import de.nachbarschaft.commands.ChapterCommand;
 import de.nachbarschaft.trigger.PlayerMoveListener;
+import de.nachbarschaft.soulweapons.SoulWeaponManager;
 
 public class Main extends JavaPlugin {
 
+    // Singleton Instance
+    private static Main instance;
+
+    // Manager
     private ChapterManager chapterManager;
+    private SoulWeaponManager soulWeaponManager;
 
     @Override
     public void onEnable() {
 
+        // Instance setzen
+        instance = this;
+
         // Chapter Manager starten
-        chapterManager = new ChapterManager(this);
+        chapterManager =
+                new ChapterManager(this);
+
+        // SoulWeapon Manager starten
+        soulWeaponManager =
+                new SoulWeaponManager();
 
         // Command registrieren
         if (getCommand("chapter") != null) {
 
-            getCommand("chapter").setExecutor(
-                    new ChapterCommand(chapterManager)
-            );
+            getCommand("chapter")
+                    .setExecutor(
+                            new ChapterCommand(
+                                    chapterManager
+                            )
+                    );
 
         }
 
         // Movement Listener registrieren
-        getServer().getPluginManager().registerEvents(
-                new PlayerMoveListener(chapterManager),
-                this
-        );
+        getServer()
+                .getPluginManager()
+                .registerEvents(
+                        new PlayerMoveListener(
+                                chapterManager
+                        ),
+                        this
+                );
 
-        getLogger().info(
-                "NachbarschaftsPlugin gestartet!"
-        );
+        getLogger()
+                .info(
+                        "NachbarschaftsPlugin gestartet!"
+                );
 
     }
 
     @Override
     public void onDisable() {
 
-        getLogger().info(
-                "NachbarschaftsPlugin gestoppt!"
-        );
+        getLogger()
+                .info(
+                        "NachbarschaftsPlugin gestoppt!"
+                );
 
+    }
+
+    // Instance Getter
+    public static Main getInstance() {
+        return instance;
+    }
+
+    // Chapter Manager Getter
+    public ChapterManager getChapterManager() {
+        return chapterManager;
+    }
+
+    // SoulWeapon Manager Getter
+    public SoulWeaponManager getSoulWeaponManager() {
+        return soulWeaponManager;
     }
 
 }
